3ab2f7a4b0cedebf1e81e66fb0211ac447036b19
FabioKlinger <bohnenpixel@gmail.com>
2025-05-20 17:20:43 +0200

    Ka wieso es nicht klappt

--- Assets/+++Workdata/Scripts/InventoryManager.cs
+++ Assets/+++Workdata/Scripts/InventoryManager.cs
@@ -27,4 +27,77 @@ public class InventoryManager : MonoBehaviour
     {
         PlayerController.InventoryAction += InventoryMenu;
     }
+
+    private void OnDisable()
+    {
+        PlayerController.InventoryAction -= InventoryMenu;
+    }
+
+    public void InventoryMenu()
+    {
+        if (inventoryContainer.activeSelf)
+        {
+            inventoryContainer.SetActive(false);
+            return;
+        }
+        
+        inventoryContainer.SetActive(true);
+
+        List<State> currentStates = GameState.Instance.GetAllStates();
+
+        for (int i = 0; i < currentStates.Count; i++) 
+        {
+            Item newItem = ItemManager.Instance.GetItemById(currentStates[i].id);
+        }
+
+        for (int i = currentStates.Count; i < slots.Count; i++)
+        {
+            slots[i].RefreshSlot();
+        }
+
+        if (currentStates.Count == 0)
+        {
+            alreadyForcedToggle = false;
+
+            ShowItemInformation();
+            for (int i = 0; i < slots.Count; i++)
+            {
+                slots[i].ForceHideHighlight();
+            }
+
+            return;
+        }
+
+        if (!alreadyForcedToggle)
+        {
+            slots[0].ForceHideHighlight();
+            alreadyForcedToggle = true;
+        }
+
+        for (int i = 0; i < slots.Count; i++)
+        {
+            if (slots[i].NoItemButIsOn())
+            {
+                slots[0].ForceToggleOn();
+                ShowItemInformation(slots[0].item);
+                break;
+            }
+        }
+    }
+
+    public void ShowItemInformation(Item item = null)
+    {
+        if (item == null)
+        {
+            itemImage.enabled = false;
+            itemHeader.SetText("");
+            itemDescription.SetText("");
+            return;
+        }
+        
+        itemImage.enabled = true;
+        itemImage.sprite = item.itemSprite;
+        itemHeader.SetText(item.itemName);
+        itemDescription.SetText(item.itemDescription);
+    }
 }
