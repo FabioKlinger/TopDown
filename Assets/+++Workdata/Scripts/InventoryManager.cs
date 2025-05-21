08031d48ab37c751ebef429ef7c788e23c7282f1
FabioKlinger <bohnenpixel@gmail.com>
2025-05-21 12:15:36 +0200

    Geht iwie

--- Assets/+++Workdata/Scripts/InventoryManager.cs
+++ Assets/+++Workdata/Scripts/InventoryManager.cs
@@ -48,6 +48,7 @@ public class InventoryManager : MonoBehaviour
         for (int i = 0; i < currentStates.Count; i++) 
         {
             Item newItem = ItemManager.Instance.GetItemById(currentStates[i].id);
+            slots[i].RefreshSlot(newItem, currentStates[i].amount);
         }
 
         for (int i = currentStates.Count; i < slots.Count; i++)
