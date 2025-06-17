5d0e1c533494b5e0f815757d3d05d497a3c27f22
FabioKlinger <bohnenpixel@gmail.com>
2025-06-17 13:34:20 +0200

    bhgnv m

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
@@ -17,33 +17,52 @@ public class UiManager : MonoBehaviour
     [SerializeField] private GameObject optionsMenuContainer;
     //....
 
+    [Header("Options Menu")] 
+    [SerializeField] private GameObject firstSelectedOption;
+
+    [SerializeField] private GameObject options_controlContainer;
+    [SerializeField] private Button controlButton;
+    [SerializeField] private GameObject options_grahpicContainer;
+    [SerializeField] private Button graphicButton;
+    [SerializeField] private GameObject options_soundContainer;
+    [SerializeField] private Button soundButton;
+
+    [FormerlySerializedAs("maxIndex")] [SerializeField] private int menuNumber;
     [Header("Button References")]
     [SerializeField] private Button optionsApplyButton;
     
     private GameObject currentMenu;
+    private GameObject currentOption;
     public int menuIndex = 0;
+    public int optionsIndex;
     #region InputSystem
 
     private InputSystem_Actions inputActions;
     private InputAction cancelAction;
-    
+    private InputAction gamepadRsAction;
+    private InputAction gamepadLsAction;
     #endregion
 
     private void Awake()
     {
         inputActions = new InputSystem_Actions();
         cancelAction = inputActions.UI.Cancel;
+        gamepadRsAction = inputActions.UI.GamepadRs;
+        gamepadLsAction = inputActions.UI.GamepadLs;
     }
 
     private void OnEnable()
     {
         inputActions.Enable();
         cancelAction.performed += CancelInput;
+        gamepadRsAction.performed += RsInput;
+        gamepadLsAction.performed += LsInput;
     }
 
     private void Start()
     {
         currentMenu = firstSelectedMenu;
+        currentOption = firstSelectedOption;
         
         if (firstSelectedButton)
         {
@@ -55,6 +74,8 @@ public class UiManager : MonoBehaviour
     {
         inputActions.Disable();
         cancelAction.performed -= CancelInput;
+        gamepadRsAction.performed -= RsInput;
+        gamepadLsAction.performed -= LsInput;
     }
 
     public void SelectButton(Button button)
@@ -68,17 +89,19 @@ public class UiManager : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(button.gameObject);
     }
 
+    public void ShowMenu(GameObject menuContainer)
+    {
+        currentMenu.SetActive(false);
+        currentMenu = menuContainer;
+        currentMenu.SetActive(true);
+    }
+    
     public void Menu_ShowMain()
     {
         menuIndex = 0;
         currentMenu.SetActive(false);
         currentMenu = mainMenuContainer;
-        mainMenuContainer.SetActive(true);
-    }
-
-    public void LoadGame()
-    {
-        SceneManager.LoadScene(1);
+        currentMenu.SetActive(true);
     }
     
     public void Menu_ShowLoad()
@@ -91,14 +114,42 @@ public class UiManager : MonoBehaviour
         menuIndex = 2;
         currentMenu.SetActive(false);
         currentMenu = optionsMenuContainer;
-        optionsMenuContainer.SetActive(true);
+        currentMenu.SetActive(true);
     }
     
     public void Menu_ShowCredits()
     {
 
     }
-
+    
+    
+    public void Options_ShowControl()
+    {
+        optionsIndex = 0;
+        currentOption.SetActive(false);
+        currentOption = options_controlContainer;
+        currentOption.SetActive(true);
+        SelectButton(controlButton);
+    }
+    
+    public void Options_ShowGraphic()
+    {
+        optionsIndex = 1;
+        currentOption.SetActive(false);
+        currentOption = options_grahpicContainer;
+        currentOption.SetActive(true);
+        SelectButton(graphicButton);
+    }
+    
+    public void Options_ShowSound()
+    {
+        optionsIndex = 2;
+        currentOption.SetActive(false);
+        currentOption = options_soundContainer;
+        currentOption.SetActive(true);
+        SelectButton(soundButton);
+    }
+    
     private void CancelInput(InputAction.CallbackContext ctx)
     {
         
@@ -123,6 +174,47 @@ public class UiManager : MonoBehaviour
             
             case 4: // quit question
                 
+                break;
+        }
+    }
+    
+    private void RsInput(InputAction.CallbackContext ctx)
+    {
+        optionsIndex++;
+
+        if (optionsIndex > menuNumber -1)
+        {
+            optionsIndex = 0;
+        }
+
+        ShowOptionMenu(optionsIndex);
+    }
+    private void LsInput(InputAction.CallbackContext ctx)
+    {
+        optionsIndex--;
+
+        if (optionsIndex < 0)
+        {
+            optionsIndex = menuNumber - 1;
+        }
+
+        ShowOptionMenu(optionsIndex);
+    }
+
+    void ShowOptionMenu(int index)
+    {
+        switch (index)
+        {
+            case 0: // options controls
+                Options_ShowControl();
+                break;
+            
+            case 1: // options graphic
+                Options_ShowGraphic();
+                break;
+            
+            case 2: // options sound
+                Options_ShowSound();
                 break;
         }
     }
